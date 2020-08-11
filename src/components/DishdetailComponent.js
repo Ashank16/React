7725158a3d3149b5360a4f5b89a777f67df1d648
Ashank16 <ashankjuyal@gmail.com>
2020-08-12 01:46:44 +0530

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Card,CardImg,CardText,CardTitle,CardBody} from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-class DishDetail extends Component {
-
-    render() { 
-        const {dish} = this.props;
+    const DishDetail = (props) => {       
+        
+        const {dish} = props;
         return ( 
             <div className="container">
                 <div className="row">
-                    {this.renderDish(dish)}
+                    <RenderDish dish={props.dish} />
                 </div>
             </div>    
         );
     }
 
-    renderDish = (dish) => {    
+    function RenderDish({dish}) { 
+        
         if (dish!=null) {        
             return (
             <React.Fragment>
@@ -30,7 +30,7 @@ class DishDetail extends Component {
                 </div>
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
-                    {this.renderComments(dish.comments)}
+                    <RenderComments comments={dish.comments} />
                 </div>
             </React.Fragment>
             );
@@ -40,7 +40,7 @@ class DishDetail extends Component {
         }
     }
 
-    renderComments = (comments) => {   
+    function RenderComments({comments}) {   
 
         if (comments!=null) {        
             const com = comments.map( (co) => {
@@ -64,14 +64,5 @@ class DishDetail extends Component {
             return(<div></div>);
         }
     }
-
-//    formatDate(date) {    
-//        const option = {year: 'numeric', month: 'short', day: 'numeric' };
-//        const date1 = new Date(date)
-//        const newdate = date1.toLocaleDateString("en-US", option)
-//        return newdate;
-//    }
     
-}
-
 export default DishDetail;
